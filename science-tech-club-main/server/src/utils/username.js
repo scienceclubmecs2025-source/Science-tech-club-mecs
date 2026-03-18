d4e30689b264bb7900e70f87476e81fe6b7b0ca1
scienceclubmecs2025-source <scienceclubmecs2025@gmail.com>
2026-03-18 16:34:42 +0530

    Delete science-tech-club-main directory

--- science-tech-club-main/server/src/utils/username.js
+++
@@ -1,9 +0,0 @@
-const buildStudentUsername = (surname, dob) => {
-  const [dd, mm, yyyy] = dob.split("-");
-  const yy = yyyy.slice(-2);
-  return `${surname}${dd}${mm}${yy}`;
-};
-
-const buildFacultyUsername = (email) => email;
-
-module.exports = { buildStudentUsername, buildFacultyUsername };
